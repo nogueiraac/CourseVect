8c84ae12f1dc587f07dc2a84e2cc5f46baa70fd2
Arthur Cesar F S Nogueira <arthur.nogueira@fitbank.com.br>
2022-05-19 08:20:19 -0300

    First Commit.

---
+++ CourseVect/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CourseVect {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Informe o número de quartos que serão ocupados: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Student[] rooms = new Student[n];
+
+            for (int i = 0; i < n; i++) {
+                Console.WriteLine("Quartos disponíveis: ");
+                for (int j = 0; j < n; j++) {
+                    if(rooms[j] == null) {
+                        Console.WriteLine($"Quarto {j} vazio");
+                    }
+                }
+                Console.Write("Informe o nome do estudante: ");
+                string name = Console.ReadLine();
+                Console.Write("Informe o e-mail do estudante: ");
+                string email = Console.ReadLine();
+                Console.Write("Informe o quarto do estudante: ");
+                int room = int.Parse(Console.ReadLine());
+        
+                rooms[room] = new Student {
+                    Name = name,
+                    Email = email,
+                    Room = room
+                };
+                Console.WriteLine("\nAlocação realizada com sucesso! \n");
+            }
+
+            Console.WriteLine("Alocação dos quartos: ");
+            for (int i = 0; i < rooms.Length; i++) {
+                Console.WriteLine(rooms[i]);
+            }
+        }
+    }
+}
---
+++ CourseVect/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CourseVect {
+    class Student {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Room { get; set; }
+
+        public Student() {
+         
+        }
+        public override string ToString() {
+            return $"Quarto {Room}: {Name}, {Email}";
+        }
+    }
+}
